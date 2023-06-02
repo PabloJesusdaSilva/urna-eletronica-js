9e39ccb038e0d534d5443ec7ff9f790b5a6a440f
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-06-02 16:05:22 -0300

    Adicionando horário de inicio e fim e adicionando alert

--- script.js
+++ script.js
@@ -7,27 +7,30 @@ function main() {
         votoEmBranco = 0,
         votoNulo = 0,
         totalVotos = 0,
-        senha,
+        senha = parseInt(prompt("Crie uma senha: ")),
         primeiroCandidato,
         segundoCandidato,
         terceiroCandidato,
         confirmaInicio,
-        confirmacaoFinal;
+        confirmacaoFinal,
+        dataHoraInicio,
+        dataHoraFim;
         
     // do while responsável por iniciar a votação    
     do {
-        senha = parseInt(prompt("Crie uma senha: "));
         primeiroCandidato = prompt("Digite o nome do primeiro candidato: ");
         segundoCandidato = prompt("Digite o nome do segundo candidato: ");
         terceiroCandidato = prompt("Digite o nome do terceiro candidato: ");
-        confirmaInicio = prompt("Deseja iniciar a votação? (S) (N)")
+        confirmaInicio = prompt("Deseja iniciar a votação? (S) (N)");
 
     }  while (confirmaInicio !== "S" && confirmaInicio !== "s");
 
+    dataHoraInicio = dataHora();
+    window.alert(dataHoraInicio);
+
     // do while responsável por contar os votos
     do {
-        console.log("| 1 |", primeiroCandidato, "\n| 2 |", segundoCandidato, "\n| 3 |", terceiroCandidato, "\n| 5 | Voto em branco \n| 8 | Voto nulo \n\nDigite a sua senha para encerrar a votação!");
-        codigoVoto = setTimeout(prompt("Digite o número do seu candidato: "), 500);
+        codigoVoto = prompt("| 1 | " + primeiroCandidato + "\n| 2 | " + segundoCandidato + "\n| 3 | " + terceiroCandidato + "\n| 5 | Voto em branco \n| 8 | Voto nulo \n\nDigite a sua senha para encerrar a votação!" + "\nDigite o número do seu candidato: ");
         
         if (codigoVoto !== null) {
             codigoVoto = parseInt(codigoVoto);
@@ -71,37 +74,41 @@ function main() {
                 break;
             default:
                 console.clear();
-                console.log("Número inválido");
+                window.alert("NÚMERO INVÁLIDO");
         }
 
     } while (confirmacaoFinal !== "S" && confirmacaoFinal !== "s");    
 
     console.clear();
-    percentual();
     candidatoVencedor();
+    percentual();
 
     // Função que define o vencedor
     function candidatoVencedor() {
         if (votoCandidato1 > votoCandidato2 && votoCandidato1 > votoCandidato3) {
-            console.log("O candidato", primeiroCandidato, "é o vencedor!");
+            window.alert("O candidato " + primeiroCandidato + " é o vencedor!");
         } else if (votoCandidato2 > votoCandidato1 && votoCandidato2 > votoCandidato3) {
-            console.log("O candidato", segundoCandidato, "é o vencedor!");
+            window.alert("O candidato " + segundoCandidato + " é o vencedor!");
         } else if (votoCandidato3 > votoCandidato1 && votoCandidato3 > votoCandidato2) {
-            console.log("O candidato", terceiroCandidato, "é o vencedor!");
+            window.alert("O candidato " + terceiroCandidato + " é o vencedor!");
         } else {
-            console.log("Empate!");
+            window.alert("Empate!");
         }
     }
 
     // função que calcula o percentual de cada candidato
     function percentual() {
-        console.log(">> VOTAÇÃO ENCERRADA <<");
-        console.log("Votos do candidato ", primeiroCandidato, " = ", votoCandidato1, Math.round((votoCandidato1 / totalVotos) * 100), "%");
-        console.log("Votos do candidato ", segundoCandidato, " = ", votoCandidato2, Math.round((votoCandidato1 / totalVotos) * 100), "%");
-        console.log("Votos do candidato ", terceiroCandidato, " = ", votoCandidato3, Math.round((votoCandidato3 / totalVotos) * 100), "%");
-        console.log("Votos em BRANCO", " = ", votoEmBranco);
-        console.log("Votos NULO", " = ", votoNulo);
-        console.log("Total de votos", " = ", totalVotos, "\n");   
+        window.alert(">> VOTAÇÃO ENCERRADA << \n" 
+                    + "Votos do candidato " + primeiroCandidato + " = " + votoCandidato1 + " " + Math.round((votoCandidato1 / totalVotos) * 100) + "%\n"
+                    + "Votos do candidato " + segundoCandidato + " = " + votoCandidato2 + " " + Math.round((votoCandidato1 / totalVotos) * 100) + "%\n"
+                    + "Votos do candidato " + terceiroCandidato + " = " + votoCandidato3 + " " + Math.round((votoCandidato3 / totalVotos) * 100) + "%\n"
+                    + "Votos do candidato " + terceiroCandidato + " = " + votoCandidato3 + " " + Math.round((votoCandidato3 / totalVotos) * 100) + "%\n"
+                    + "Votos em BRANCO" + " = " + votoEmBranco   + "\n" 
+                    + "Votos NULO" + " = " + votoNulo + "\n"
+                    + "Total de votos" + " = " + totalVotos + "\n");
+
+        dataHoraFim = dataHora();
+        window.alert(dataHoraFim);
     }
 
     // função para adicionar audio a cada voto
@@ -109,5 +116,13 @@ function main() {
         let audio = new Audio("confirma-urna.mp3");
 
         audio.play();
+    }
+
+    // funcão para indicar o horário
+    function dataHora() {
+        let dataAtual = new Date().toLocaleDateString();
+        let horaAtual = new Date().toLocaleTimeString();
+
+        return dataAtual + " " + horaAtual;
     }
 }
